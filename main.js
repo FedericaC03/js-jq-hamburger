93acaaa54fca4082fa3c08fded1abd6c648a9d8d
FedericaC03 <fdrk03@hotmail.it>
2020-09-01 15:59:13 +0200

    fine esercizio con una seconda opzione

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
-//VARIABILI
-var hamburger = $(".fa-bars");
-var close = $(".fa-times");
-var visible = false;
+//OPZIONE 1
+// VARIABILI
+// var hamburger = $(".fa-bars");
+// var close = $(".fa-times");
+// var visible = false;
+//
+// //GENERO UNA FUNZIONE PER MOSTRARE IL MENU PRINCIPALE CLICCANDO SULL'HAMBURGER
+// hamburger.click(
+//   function () {
+//     if (visible == false) {
+//       $(".hamburger-menu").fadeIn(1000);
+//       visible = true;
+//     }
+//   }
+// );
+//
+// //GENERO UNA FUNZIONE PER NASCONDERE IL MENU PRINCIPALE CLICCANDO SULLA X
+// close.click(
+//   function () {
+//     if (visible == true) {
+//       $(".hamburger-menu").fadeOut(1000);
+//       visible = false;
+//     }
+//   }
+// );
 
-//GENERO UNA FUNZIONE PER MOSTRARE IL MENU PRINCIPALE CLICCANDO SULL'HAMBURGER
-hamburger.click(
+
+//OPZIONE 2
+//CREO LE VARIABILI
+var close = $(".close")
+var bars = $(".header-right");
+var hamburger = $(".hamburger-menu");
+
+//GENERO UNA FUNZIONE PER AGGIUNGERE LA CLASSE ACTIVE
+bars.click(
   function () {
-    if (visible == false) {
-      $(".hamburger-menu").fadeIn(1000);
-      visible = true;
+    hamburger.addClass("active");
     }
-  }
 );
 
-//GENERO UNA FUNZIONE PER NASCONDERE IL MENU PRINCIPALE CLICCANDO SULLA X
+//GENERO UNA FUNZIONE PER RIMUOVERE LA CLASSE ACTIVE
 close.click(
   function () {
-    if (visible == true) {
-      $(".hamburger-menu").fadeOut(1000);
-      visible = false;
-    }
+    hamburger.removeClass("active");
   }
 );
